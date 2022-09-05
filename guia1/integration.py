d948df713874a1575be18c97512b28c9005ca426
mu-coder69 <106639871+mu-coder69@users.noreply.github.com>
2022-09-04 22:17:49 -0300

    update

--- guia1/integration.py
+++ guia1/integration.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def trap_int(INTERVAL, f, N=1_000_000, chunk_size=1_000_000, dtype=np.float32):
-    h = (INTERVAL[1] - INTERVAL[0]) / N
+    h = (INTERVAL[1] - INTERVAL[0]) / (N -1)
     # get the intervals and terms of the chunks
     intervals = chunkify(INTERVAL, N, chunk_size)
     integral = 0
@@ -13,30 +13,29 @@ def trap_int(INTERVAL, f, N=1_000_000, chunk_size=1_000_000, dtype=np.float32):
         y_values = f(x_values)
         weights = np.full(len(y_values), h, dtype=dtype)
         if i == 0:
-            weights[0] /= 2
-        if i == len(intervals):
-            weights[-1] /= 2
+            weights[0] = h/2
+        if i == len(intervals) -1:
+            weights[-1] = h/2
         integral += weights @ y_values.T
     return integral
 
 def simpson_int(INTERVAL, f, N=1_000_000, chunk_size=1_000_000, dtype=np.float32):
     N = N -1 if N % 2 == 0 else N
-    h = (INTERVAL[1] - INTERVAL[0]) / (N +1)
+    h = (INTERVAL[1] - INTERVAL[0]) / (N -1)
     # get the intervals and terms of the chunks
     intervals = chunkify(INTERVAL, N, chunk_size)
     integral = 0
     for i in range(len(intervals)):
         a, b, terms, endpoint = intervals[i]
-        # x_values = np.linspace(a, b, terms +1, endpoint=endpoint, dtype=dtype)
-        x_values = np.arange(a, b, h)
+        x_values = np.linspace(a, b, terms, endpoint=endpoint, dtype=dtype)
+        # x_values = np.arange(a, b, h)
         y_values = f(x_values)
         weights = np.full(len(y_values), 4*h/3, dtype=dtype)
         weights[::2] = 2*h/3
         if i == 0:
             weights[0] = h/3
-        if i == len(intervals):
+        if i == len(intervals) -1:
             weights[-1] = h/3
-        
         integral += weights @ y_values.T
     return integral
 
@@ -45,7 +44,7 @@ def chunkify(INTERVAL, N, chunk_size=1_000_000):
     # if "N" is not an integer multiple of "chunk_size", then store
     # the remainder terms in "remainder"
     CHUNKS, remainder = divmod(N, chunk_size)
-    h = (INTERVAL[1] - INTERVAL[0]) / N
+    h = (INTERVAL[1] - INTERVAL[0]) / (N -1)
     # "INTERVAL_STEP" is the range of the intervals
     INTERVAL_STEP = chunk_size * h
     intervals = []
@@ -70,18 +69,18 @@ def chunkify(INTERVAL, N, chunk_size=1_000_000):
         intervals.append((a, b, terms, endpoint))
     return intervals
         
-N = tuple(range(10, 10_000_000, 100_000))
+N = tuple(range(3, 10_000_000, 100_000))
 errors = []
 errors2 = []
 errors3 = []
 errors4 = []
-dtype = np.float64
+dtype = np.float32
 for n in N:
     trap1 = trap_int([0, 1], lambda x: x*x*x*x*x, N=n, dtype=dtype)
     trap2 = trap_int([0, 1], lambda x: 3*x*x*x, N=n, dtype=dtype)
     simp1 = simpson_int([0, 1], lambda x: x*x*x*x*x, N=n, dtype=dtype)
     simp2 = simpson_int([0, 1], lambda x: 3*x*x*x, N=n, dtype=dtype)
-    print(f"N: {n}, simp1: {simp1}, simp2: {simp2}")
+    print(f"N: {n}")
     errors.append(abs(1/6 - trap1))
     errors2.append(abs(3/4 - trap2))
     errors3.append(abs(1/6 - simp1))
@@ -92,11 +91,11 @@ errors3 = errors3 / max(errors3)
 errors4 = errors4 / max(errors4)
 plt.loglog(N, errors, label="$x^5$", c="k")
 plt.loglog(N, errors2, label="$1/2 x^{-1/2}$", c="k")
-# plt.title("Trapeze integral")
-# plt.legend()
-# plt.show()
+plt.title("Trapeze integral")
+plt.legend()
+plt.show()
 plt.loglog(N, errors3, label="$x^5$", c="r")
 plt.loglog(N, errors4, label="$1/2 x^{-1/2}$", c="r")
-# plt.title("Simpson integral")
+plt.title("Simpson integral")
 plt.legend()
 plt.show()
