3fd5fcaeff28e82fa98d8e85cfd65729fe29fa82
mu-coder69 <lucianokoval21@gmail.com>
2022-11-07 21:07:22 -0300

    some changes

--- guia2/pendulumODE.py
+++ guia2/pendulumODE.py
@@ -1,4 +1,5 @@
 import numpy as np
+# import sympy as sp
 from modules.ODEsolver import RK4
 import modules.writer as w
 from time import perf_counter, localtime, strftime
@@ -17,10 +18,10 @@ def pendulum(t_interval: list, init_cond: list, params: list, h=1E-3) -> list:
                         lambda y: pendulum_eqs(y, B, W, F),
                         h)
 
-        if sys_vector[0] < -np.pi:
-            sys_vector[0] += 2*np.pi
-        elif sys_vector[0] > np.pi:
-            sys_vector[0] -= 2*np.pi
+        # if sys_vector[0] < -np.pi:
+        #     sys_vector[0] += 2*np.pi
+        # elif sys_vector[0] > np.pi:
+        #     sys_vector[0] -= 2*np.pi
 
         pos[i], vel[i], _ = sys_vector
     return np.array([t, pos, vel])
@@ -37,16 +38,17 @@ def pendulum_eqs(y, B=0, W=1, F=0):
 
 if __name__ == '__main__':
 
-    pos_0 = 0.2
+    e = 1E-3
+    pos_0 = [0.2, 0.2 + e]
     vel_0 = 0 
     B = 0.5
     W = 2/3
-    F = [0.9, 1.075, 1.12, 1.2, 1.4, 1.45, 1.47, 1.5, 1.51]
-    t_interval = (0, 10_000)
-    init_cond = (pos_0, vel_0)
+    F = 1.2
+    t_interval = (0, 5_000)
+    # init_cond = (pos_0, vel_0)
 
     import concurrent.futures
-    arguments = np.array([(t_interval, init_cond, (B, W, F[i]), 1E-2) for i  in range(len(F))], dtype=object)
+    arguments = np.array([(t_interval, (pos_0[i], vel_0), (B, W, F), 1E-3) for i  in range(len(pos_0))], dtype=object)
 
     print(strftime("%Y-%m-%d %H:%M:%S", localtime()))
     s = perf_counter()
@@ -62,15 +64,15 @@ if __name__ == '__main__':
     headers = ['time', 'pos', 'vel', 'energy']
     i = 0
     for result in results:
-        print(f'file {i +1}/{len(F)}')
+        print(f'file {i +1}/{len(pos_0)}')
         result = np.concatenate([result.T, 
                                 get_energy(result[2, :], result[1, :]).reshape(-1, 1)], 
                                 axis=1)
         w.write(result, 
-                name='pendulumODE' + f'({F[i]})', 
+                name='pendulumODE' + f'({pos_0[i]}-{F})', 
                 header=headers, 
                 split=True, 
                 keep=1, 
-                path=r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\0')
+                path=r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\00')
         i += 1
 
--- guia2/pendulumOptimizer.py
+++ guia2/pendulumOptimizer.py
@@ -2,29 +2,32 @@ import os
 import numpy as np
 import modules.writer as w
 
-path = r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\1'
-path2 = r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\2'
+path = r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\0'
+path2 = r'C:\Users\luciano\Documents\GitHub\computational-physics\guia2\data\3'
 
 i = 1
+files = ['pendulumODE(0.2)2.csv',
+         'pendulumODE(0.201)2.csv']
 for dataset in os.listdir(path):
-    print(f'file {i}/{len(os.listdir(path))}')
-    data = np.genfromtxt(open(os.path.join(path, dataset), 'rb'), 
-                    delimiter=" ", 
-                    names=True,
-                    dtype=np.float64)
+    if dataset in files:
+        print(f'file {i}/{len(os.listdir(path))}')
+        data = np.genfromtxt(open(os.path.join(path, dataset), 'rb'), 
+                        delimiter=" ", 
+                        names=True,
+                        dtype=np.float64)
 
-    headers = data.dtype.names
-    data = np.array([data[headers[0]], data[headers[1]]]).T
+        headers = data.dtype.names
+        data = np.array([data[headers[0]], data[headers[1]]]).T
 
-    dump = 90 # %
-    rows_to_dump = int(data.shape[0]*dump / 100)
-    data = w.dump(data, rows_to_dump)
+        dump = 0 # %
+        rows_to_dump = int(data.shape[0]*dump / 100)
+        data = w.dump(data, rows_to_dump)
 
-    remaining_data = 10 # %
-    data_to_keep = int((data.shape[0] - 1)*remaining_data / 100)
-    data = w.optimize(data, int((data.shape[0] - 1) / data_to_keep))
+        remaining_data = 30 # %
+        data_to_keep = int((data.shape[0] - 1)*remaining_data / 100)
+        data = w.optimize(data, int((data.shape[0] - 1) / data_to_keep))
 
-    name = os.path.splitext(dataset)[0]
-    w.write(data, header=headers, name=name, path=path2)
+        name = os.path.splitext(dataset)[0]
+        w.write(data, header=headers, name=name, path=path2)
 
     i += 1
